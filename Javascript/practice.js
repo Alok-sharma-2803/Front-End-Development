7de215595e7284732fd683de6b5408a2216efb01
Alok-sharma-2803 <alok.ggg@gmail.com>
2025-07-25 08:06:39 +0530

    intial commit

---
+++ Javascript/practice.js
@@ -0,0 +1,25 @@
+const product = {
+    productName: "Parker Jotter Standard CT Ball Pen",
+    productColor: "Black",
+    productRating: 4,
+    reviewCount: 7002,
+    hasActiveDeal: true,
+    percentOff: 5,
+    productMRP: 285,
+};
+
+
+const profile = {
+    firstName: "Alok",
+    lastName: "Sharma",
+    isProfileVerified: true,
+    postsCount: 195,
+    followersCount: 569000,
+    followingCount: 4,
+    jobRole: "Data Analyst",
+    currentWorkPlace: "Sprinklr",
+    lastWorkplace1: "Udaan",
+    lastWorkPlace2: "Vedantu",
+    lastWorkPlace3: "Cure.Fit",
+    objective: "To complete my M.S. in Analytics"
+};
